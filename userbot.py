59a8cade3ab1f5561fec78585fd993dfa0ae2816
punkhere <79733447+punkhere@users.noreply.github.com>
2021-03-15 15:45:55 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -8,3 +8,22 @@ print('>>> USERBOT STARTED')
 idle()
 app.stop()
 print('\n>>> USERBOT STOPPED')
+from pyrogram import Client, idle
+
+api_id = 941514
+api_hash = "2b8e24188631929de4bda0c008ba1685"
+
+plugins = dict(
+    root="plugins",
+    include=[
+        "vc.player",
+        "ping"
+    ]
+)
+
+app = Client("tgvc", api_id, api_hash, plugins=plugins)
+app.start()
+print('>>> USERBOT STARTED')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED')
